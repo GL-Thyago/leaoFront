8f8206f38e479b99d882be14e74629f648e2f313
Raul Oliveira <134628269+GL-Thyago@users.noreply.github.com>
2024-02-24 17:06:22 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  // baseURL: 'https://leao-leao-back.jx1fyp.easypanel.host/api',
-  baseURL: 'http://192.168.15.3:8000/api',
+  baseURL: 'https://leao-leao-back.jx1fyp.easypanel.host/api',
+  // baseURL: 'http://192.168.15.3:8000/api',
 
   headers: {
     Accept: "application/json",
