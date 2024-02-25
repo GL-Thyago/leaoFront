dfc3614f37162c8eb7e5ff4e1e8b9b0dbdae831c
Raul Oliveira <134628269+GL-Thyago@users.noreply.github.com>
2024-02-25 19:57:27 -0300

    deploy1

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  // baseURL: 'https://leao-leao-back.jx1fyp.easypanel.host/api',
-  // img: 'https://leao-leao-back.jx1fyp.easypanel.host',
+  baseURL: 'https://leao-leao-back.jx1fyp.easypanel.host/api',
+  img: 'https://leao-leao-back.jx1fyp.easypanel.host',
 
-  baseURL: 'http://192.168.15.22:8000/api',
-  img: 'http://192.168.15.22:8000',
+  // baseURL: 'http://192.168.15.22:8000/api',
+  // img: 'http://192.168.15.22:8000',
 
 
   headers: {
