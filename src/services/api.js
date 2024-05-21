27966dc7042ebef4ee1b31bc2dd021b01f612a0a
Raul Oliveira <134628269+GL-Thyago@users.noreply.github.com>
2024-05-21 13:43:21 -0300

    teste sem content

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,14 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  baseURL: 'https://uni-leao-rifa.jagkzv.easypanel.host/api',  
-  img: 'https://uni-leao-rifa.jagkzv.easypanel.host',  
-
-  // baseURL: 'http://192.168.15.164:8000/api', 
-  // img: 'http://192.168.15.164:8000', 
+  baseURL: 'https://uni-leao-back.jagkzv.easypanel.host/api',  
+  img: 'https://uni-leao-back.jagkzv.easypanel.host',  
   // baseURL: 'http://127.0.0.1:8000/api', 
   // img: 'http://127.0.0.1:8000', 
-// 
 
   headers: {
     Accept: "application/json",
-    // "Content-Type": "application/json",
+    "Content-Type": "application/json",
   },
 });
 
